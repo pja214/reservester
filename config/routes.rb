6e6842e04a4f615d8a5aa50693f0950328112780
Patrick Adduci <adduci92@gmail.com>
2014-06-26 12:54:29 -0400

    Add scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Reservester::Application.routes.draw do
+  resources :restaurants
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
